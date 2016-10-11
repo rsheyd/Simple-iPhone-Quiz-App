46db443f052552eddde7acf786b437a1a036a58f
Roman Sheydvasser <s.roman@gmail.com>
2016-10-11 09:34:53 -0700

    Added readme

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["What is cognac made of?", "What is 7+7?",
+                               "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes","14","Montpelier"]
+    var currentQuestionIndex: Int = 0
+
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        answerLabel.text = "???"
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count { currentQuestionIndex = 0 }
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+
+    @IBAction func showAnswer(sender: AnyObject) {
+        answerLabel.text = answers[currentQuestionIndex]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple quiz app for iOS, coded in Swift 3/Xcode 8.
